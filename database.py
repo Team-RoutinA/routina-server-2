a76f38878ce6e45572b2a5b30ce63e2c29a1ca8d
altakori <seollal25@snu.ac.kr>
2025-06-01 19:50:47 +0900

    merge5

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:비번@localhost:포트/데베이름"
+DATABASE_URL = "mysql+pymysql://root:4014@localhost:3306/project"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
